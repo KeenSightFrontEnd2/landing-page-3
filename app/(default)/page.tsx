b08b3c571fcedf344135b89e8e55cdaf1129558d
kabaskill <oguzkabasakal@gmail.com>
2024-06-05 03:49:45 +0200

    metadata update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,13 +1,13 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
-}
+  title: "Landing Page-4",
+  description: "Landding page 4",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Newsletter from "@/components/newsletter";
+import Zigzag from "@/components/zigzag";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -18,5 +18,5 @@ export default function Home() {
       <Testimonials />
       <Newsletter />
     </>
-  )
+  );
 }
